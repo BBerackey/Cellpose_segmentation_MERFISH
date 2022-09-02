f1791c094dfead982cf1ba10dd9ab0fe9efbb00f
Bereket <tesfayebereket612@gmail.com>
2022-09-02 15:00:29 -0700

    dapi readar

---
+++ dapi_reader.py
@@ -0,0 +1,24 @@
+import numpy as np
+import dask_image.imread
+import concurrent.futures
+
+def dapi_reader(reader_func_input):
+    # example file_path = 'U:/Lab/MERFISH_Imaging data/Kim2_202112171955_12172021TREM2-5x12Mo300GP_VMSC00101/region_0/'
+    file_path,z_idx = reader_func_input
+    dapi_dask = dask_image.imread.imread(file_path + 'images/mosaic_DAPI'+'_z'+str(z_idx)+'.tif')
+    # dapi_dask = dask_image.imread.imread('U:/Lab/MERFISH_Imaging data/Kim2_202112171955_12172021TREM2-5x12Mo300GP_VMSC00101/region_0/images/mosaic_DAPI'+'_z'+str(z_idx)+'.tif')
+    dapi_imag = dapi_dask.compute()[0,:,:]
+    print(f'done reading layer {z_idx}')
+    return dapi_imag
+
+def parallel_dapi_reader(file_path):
+    all_Z_DAPI = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        reader_func_input = [(file_path,z_idx) for z_idx in range(7)]
+        results = executor.map(dapi_reader,reader_func_input)
+        for f in results:
+            all_Z_DAPI.append(f)
+        # results = [executor.submit(dapi_reader, z_idx) for z_idx in range(3)]
+    # for f in concurrent.futures.as_completed(results):
+    #     all_Z_DAPI.append(f.result())
+    return all_Z_DAPI
