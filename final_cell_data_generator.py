0b0abfa7dd8d8f327aa06fb7b6ea843c4609588a
Bereket <tesfayebereket612@gmail.com>
2022-09-14 15:15:54 -0700

    changed the method for aligning cell center and saved contour of all cells with in fov as nested dictionary

--- final_cell_data_generator.py
+++ final_cell_data_generator.py
@@ -3,16 +3,23 @@ import pandas as pd
 import numba
 from numba.typed import List
 import pdb
+from numba.typed import Dict
+from numba.core import types
 
 @numba.jit(nopython=True)
-def numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,cell_idx_list,all_col_index):
+def numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,all_cell_idx_list,all_dict,all_col_index):
     all_numba_output = []
     center_x_col, center_y_col, x_max_col, x_min_col, y_max_col, y_min_col = all_col_index
+    cell_idx_list = all_cell_idx_list[0] # idx of cells in the 0th z-layer
+
+
     for idx in range(len(cell_idx_list)):
         cell_idx = cell_idx_list[idx]
+        algining_cell_dict = all_dict[idx]
+
         center_datatype = type(numpy_cell_meta_data[0][idx,x_max_col])
-        xy_max = np.zeros((7, 2), dtype=center_datatype)
-        xy_min = np.zeros((7, 2),dtype = center_datatype)
+        xy_max = np.zeros((len(numpy_cell_by_gene), 2), dtype=center_datatype)
+        xy_min = np.zeros((len(numpy_cell_by_gene), 2),dtype = center_datatype)
 
         # if isinstance(idx,list): # ***note isinstance is not supported in numba
         #     raise Exception('error: cell index duplicated')
@@ -27,24 +34,25 @@ def numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,cell_idx_list,
         xy_min[0, 0] = numpy_cell_meta_data[0][idx,x_min_col]
         xy_min[0, 1] = numpy_cell_meta_data[0][idx,y_min_col]
     #
-        for z_idx in range(1, 7):
+        for z_idx in range(1, len(numpy_cell_by_gene)):
             all_centerx = numpy_cell_by_gene[z_idx][:,center_x_col]
             all_centery = numpy_cell_by_gene[z_idx][:,center_y_col]
             diff1 = (all_centerx - (center_x * np.ones(all_centerx.shape)))
             diff1[diff1<0] = -1*diff1[diff1<0] # taking the absolute value
-            diff1_min = diff1.min()
-            diff1 = diff1 - (diff1_min * np.ones(diff1.shape))
-            cond1 = diff1.astype('int') == 0
+            cond1 = diff1 < 2 # only cells with centroid with less than 2 um are considered to align
+
+            # diff1_min = diff1.min()
+            # diff1 = diff1 - (diff1_min * np.ones(diff1.shape))
+            # cond1 = diff1.astype('int') == 0
 
             diff2 = (all_centery - (center_y * np.ones(all_centery.shape)))
             diff2[diff2 < 0] = -1 * diff2[diff2 < 0] # taking the absolute value
-            diff2_min = diff2.min()
-            diff2 = diff2 - (diff2_min * np.ones(diff2.shape))
-            cond2 = diff2.astype('int') == 0
+            cond2 = diff2 < 2
+            # diff2_min = diff2.min()
+            # diff2 = diff2 - (diff2_min * np.ones(diff2.shape))
+            # cond2 = diff2.astype('int') == 0
 
             cond = cond1 & cond2
-
-    #         # turn into absolute value
 
             # apply the condition, numba does not support 2D logical indexing
             if cond.any(): # if there is atleast one True, i.e at least one aligning cell
@@ -60,6 +68,12 @@ def numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,cell_idx_list,
 
                             temp_xy_min[0, 0] = np.max(np.array([temp_xy_min[0,0],numpy_cell_meta_data[z_idx][log_cell_idx[0][log_idx],x_min_col]]))
                             temp_xy_min[0, 1] = np.max(np.array([temp_xy_min[0,1],numpy_cell_meta_data[z_idx][log_cell_idx[0][log_idx],y_min_col]]))
+                            # record the aligning cells
+                            if log_idx == 0:
+                                algining_cell_dict[cell_idx]['zIndex_' + str(z_idx)] = List([all_cell_idx_list[z_idx][log_cell_idx[0][log_idx]]])
+                            else:
+                                algining_cell_dict[cell_idx]['zIndex_' + str(z_idx)].append(all_cell_idx_list[z_idx][log_cell_idx[0][log_idx]])
+
                         xy_min[z_idx,0] = temp_xy_min[0,0]
                         xy_min[z_idx, 1] = temp_xy_min[0, 1]
 
@@ -69,7 +83,7 @@ def numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,cell_idx_list,
                 break # if no cell algins to the next first z-layer, i.e. z_idx =1
                          # we do not expect cells in the other layer to align, so skip this
                          # loop to save computation time
-        all_numba_output.append((csum_gene_array, cell_idx, xy_max, xy_min))
+        all_numba_output.append((csum_gene_array, cell_idx, xy_max, xy_min,algining_cell_dict))
     return all_numba_output
 
 
@@ -83,12 +97,25 @@ def final_data_generator(func_input):
             gene_panel = list(all_cell_gene_data[0].columns)[2:]
             hight_value = np.array([1.5, 1.5, 1.5, 1.5, 1.5, 1.5])
 
-            temp_cell_idx_list = list(all_cell_gene_data[0].index)
-            # cell_idx_list = list(all_cell_gene_data[0].index)
-
             # create a typed list to make it compatible with numba
-            cell_idx_list = List()
-            [cell_idx_list.append(cell_idx) for cell_idx in temp_cell_idx_list]
+            all_cell_idx_list = List() # this will be a nested list containing all cell_idx
+            for z_idx in range(len(all_cell_gene_data)):
+                cell_idx_list = List()
+                temp_cell_idx_list = list(all_cell_gene_data[z_idx].index)
+                [cell_idx_list.append(cell_idx) for cell_idx in temp_cell_idx_list]
+                all_cell_idx_list.append(cell_idx_list)
+
+            all_dict = List()
+            for zeroth_cell_idx in all_cell_idx_list[0]:
+                algining_cell_dict = Dict.empty(key_type=types.unicode_type,
+                                                    value_type=types.DictType(types.unicode_type,
+                                                                              types.ListType(types.unicode_type)))
+                algining_cell_dict[zeroth_cell_idx] = Dict.empty(key_type=types.unicode_type,
+                                                       value_type=types.ListType(types.unicode_type))
+                all_dict.append(algining_cell_dict)
+
+                # cell_idx_list = list(all_cell_gene_data[0].index)
+
 
             meta_column_list = list(all_cell_meta_data[0].columns)
             gene_column_list = list(all_cell_gene_data[0].columns)
@@ -128,12 +155,24 @@ def final_data_generator(func_input):
             # numpy_cell_meta_data = List(temp_numpy_cell_meta_data)
             # numpy_cell_by_gene = List(temp_numpy_cell_by_gene)
             fov_processed = all_cell_meta_data[0]['fov'].max()
-            all_numba_outputs = numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,cell_idx_list,all_col_idx)
+            all_numba_outputs = numba_func_cell_merge(numpy_cell_by_gene,numpy_cell_meta_data,all_cell_idx_list,all_dict,all_col_idx)
+
 
             final_cell_by_gene = []
             final_cell_meta_data = []
+            fov_cell_contour = {}
+            fov_cell_contour[fov_processed] = []
             for numba_output in all_numba_outputs:
-                csum_gene_array,idx,xy_max,xy_min = numba_output
+                csum_gene_array,idx,xy_max,xy_min,aligining_cell_dict = numba_output
+                # convert the typed dictionary back to normal dictionary
+                temp_dict = {}
+                for cell_main_dict_key in aligining_cell_dict.keys():
+                    temp_dict[cell_main_dict_key] = {}
+                    for cell_sub_dict in aligining_cell_dict[cell_main_dict_key].keys():
+                        temp_dict[cell_main_dict_key][cell_sub_dict] = [x for x in aligining_cell_dict[cell_main_dict_key][cell_sub_dict]] # this will convert it into normal list
+
+
+                fov_cell_contour[fov_processed].append(temp_dict)
                 csum_gene_array = np.reshape(csum_gene_array, [1, len(gene_panel)])
                 final_cell_by_gene.append(pd.DataFrame(csum_gene_array, index=[idx], columns=gene_panel))
 
@@ -154,8 +193,7 @@ def final_data_generator(func_input):
 
             final_cell_by_gene = pd.concat(final_cell_by_gene)
             final_cell_meta_data = pd.concat(final_cell_meta_data)
-            # pdb.set_trace()
 
             print(f'Done generating cell data for fov: {fov_processed}')
-            return final_cell_by_gene, final_cell_meta_data
+            return final_cell_by_gene, final_cell_meta_data ,fov_cell_contour
 
